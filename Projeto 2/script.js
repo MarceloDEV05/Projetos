e435ea97df44abed56c52efbe91a94c1679c09aa
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-11-16 17:53:15 -0300

    resolvendo ultimos problemas

--- Projeto 2/script.js
+++ Projeto 2/script.js
@@ -4,20 +4,26 @@ var sec = 0;
 var timer
 
 
-
-
 function start(){
-    cronometro()
-    timer = setInterval(cronometro, 1000)
+   
+    if(!timer){
+        cronometro()
+        timer = setInterval(cronometro, 1000)
+    }
 
 }
 
 function pause(){
     clearInterval(timer)
+    
 }
 
 function reset(){
-    clearInterval(timer)
+    clearInterval(cronometro)
+    timer = null
+    hora = 0
+    mnt = 0
+    sec = 0
     document.getElementById('tempo').innerText = '00:00:00'
 }
 function cronometro(){
