9adc1abc14e4cbcc5f5785dc24c63045aa4c2896
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-12-12 14:54:35 -0300

    drink water

---
+++ drink water/script.js
@@ -0,0 +1,15 @@
+let input = document.querySelector('input')
+let btn = document.querySelector('button')
+
+btn.addEventListener('click', verificarLitros)
+
+function verificarLitros(){
+    let peso = input.value
+    peso = peso * 35
+    let litros = Math.floor(peso / 1000)
+    let mililitros = peso % 100
+    let resultado = document.getElementById('resultado')
+    resultado.innerHTML = "De acordo com seu peso, você deve consumir " + litros + " litros e " + mililitros + "ml de água por dia!"
+
+}
+
