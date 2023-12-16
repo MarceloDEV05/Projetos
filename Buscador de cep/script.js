6ee06c4c1271e3fcaecd6996c8bca4379e657c2b
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-12-15 23:09:51 -0300

    buscador de cep - integração com api

--- Buscador de cep/script.js
+++ Buscador de cep/script.js
@@ -1,2 +1,23 @@
-//"https://viacep.com.br/ws/"
+let api = "https://cdn.apicep.com/file/apicep/06233-030.json"
 
+ async function buscarCep(){
+
+    
+    const response = await fetch(api,{
+        method:"GET"
+    })
+    
+    const data = await response.json()
+    console.log(data)
+
+    let p = document.getElementById('buscado')
+
+    let apiInfo = `
+    Cep:${data.code}
+    Cidade:${data.city}
+    Estado:${data.state}
+    `
+    p.innerHTML = apiInfo
+    
+}
+buscarCep()
