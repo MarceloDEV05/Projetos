c842d0b1963cd4258c4e8c037dbac71f55e2a82b
Marcelo Cardoso <140186230+MarceloDEV05@users.noreply.github.com>
2023-12-16 01:16:48 -0300

    Buscador de CEP - final Commit

--- Buscador de cep/script.js
+++ Buscador de cep/script.js
@@ -1,23 +1,28 @@
-let api = "https://cdn.apicep.com/file/apicep/06233-030.json"
+let span = document.getElementById('buscador')
+let input = document.getElementById('inputCep')
+span.addEventListener('click', buscarCep)
 
- async function buscarCep(){
 
-    
-    const response = await fetch(api,{
-        method:"GET"
-    })
-    
+
+async function buscarCep() {
+
+    let cep = input.value
+
+
+    const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+
     const data = await response.json()
     console.log(data)
 
     let p = document.getElementById('buscado')
 
     let apiInfo = `
-    Cep:${data.code}
-    Cidade:${data.city}
-    Estado:${data.state}
+    <strong>Cep:${data.cep}<br>
+    Cidade:${data.localidade}<br>
+    Estado:${data.uf}
     `
     p.innerHTML = apiInfo
+
+
     
 }
-buscarCep()
